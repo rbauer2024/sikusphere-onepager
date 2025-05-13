e276caa3fa234f55cb068ccbf89a20b006fcbce3
rbauer2024 <168412926+rbauer2024@users.noreply.github.com>
2025-05-13 15:15:40 +0200

    Update ModelViewer.tsx

--- components/ModelViewer.tsx
+++ components/ModelViewer.tsx
@@ -1,3 +1,4 @@
+/// <reference types="@react-three/fiber" />
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Environment } from '@react-three/drei'
 import { Suspense } from 'react'
